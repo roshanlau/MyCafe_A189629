fcc44899ce18b9ec1563c3943c7db3eac46c7a0b
Roshan Lau Jee Van <roshanlau@gmail.com>
2023-10-21 21:43:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MyCafe_A189629
+ Year 3 Sem 1 Mobile Programmign Lab 
---
+++ app/src/main/java/com/example/mycafe_a189629/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.mycafe_a189629;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTitle;
+    EditText etName;
+    TextView tvDrink;
+    TextView tvQuantity;
+    Button btnAdd;
+    Button btnMinus;
+    Button btnCheckout;
+
+    int quantity;
+    String name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvTitle = findViewById(R.id.tv_title);
+        etName = findViewById(R.id.et_name);
+        tvDrink = findViewById(R.id.tv_drink);
+        tvQuantity = findViewById(R.id.tv_quantity);
+        btnAdd = findViewById(R.id.btn_add);
+        btnMinus = findViewById(R.id.btn_minus);
+        btnCheckout = findViewById(R.id.btn_checkout);
+
+        quantity = 0;
+        name = "";
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quantity++;
+                tvQuantity.setText(""+quantity);
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quantity <= 0){
+                    Toast.makeText(MainActivity.this, "Cannot be less than 0.", Toast.LENGTH_SHORT).show();
+                }else{
+                    quantity--;
+                    tvQuantity.setText(""+quantity);
+                }
+            }
+        });
+
+        btnCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = etName.getText().toString();
+                if(name == "" && quantity <= 0){
+                    Toast.makeText(MainActivity.this, "Please Enter Appropriate Name or Amount of Drink.", Toast.LENGTH_SHORT).show();
+                }else if(name == ""){
+                    Toast.makeText(MainActivity.this, "Please Enter Appropriate Name.", Toast.LENGTH_SHORT).show();
+                }else if(quantity <= 0){
+                    Toast.makeText(MainActivity.this, "Please Enter Appropriate Amount of Drink.", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Thank You " + name + " for order: " + quantity + " Latte.", Toast.LENGTH_SHORT).show();
+                    etName.setText("");
+                    quantity = 0;
+                    tvQuantity.setText(""+quantity);
+
+                    Intent intent = new Intent(MainActivity.this, OrderDetailActivity.class);
+                    startActivity(intent);
+
+
+                }
+
+            }
+        });
+
+    }
+}
