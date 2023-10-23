63b7493f914f05faae257768bded1a3774307ddd
Roshan Lau Jee Van <roshanlau@gmail.com>
2023-10-23 20:22:47 +0800

    Added intent for call, web, email button

--- app/src/main/java/com/example/mycafe_a189629/OrderDetailActivity.java
+++ app/src/main/java/com/example/mycafe_a189629/OrderDetailActivity.java
@@ -3,6 +3,7 @@ package com.example.mycafe_a189629;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -43,17 +44,43 @@ public class OrderDetailActivity extends AppCompatActivity implements OnClickLis
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_Call_OrderActivity:
-                Toast.makeText(OrderDetailActivity.this, "Btn Call", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_Web_OrderActivity:
-                Toast.makeText(OrderDetailActivity.this, "Btn Web", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_Email_OrderActivity:
-                Toast.makeText(OrderDetailActivity.this, "Btn Email", Toast.LENGTH_SHORT).show();
-                break;
-        }
+        if(v.getId() == R.id.btn_Call_OrderActivity){
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+            callIntent.setData(Uri.parse("tel: 012345678"));
 
+            if(callIntent.resolveActivity(getPackageManager()) == null){//verify whether the device can receive the following intent
+                Toast.makeText(OrderDetailActivity.this, "Sorry, no App can handle this action and data", Toast.LENGTH_SHORT).show();
+            }else{
+                startActivity(callIntent);
+            }
+
+        }else if(v.getId() == R.id.btn_Web_OrderActivity){
+
+            Uri webpage = Uri.parse("http://www.google.com");
+
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+
+            if(webIntent.resolveActivity(getPackageManager()) == null){//verify whether the device can receive the following intent
+                Toast.makeText(OrderDetailActivity.this, "Sorry, no App can handle this action and data", Toast.LENGTH_SHORT).show();
+            }else{
+                startActivity(webIntent);
+            }
+
+        }else if(v.getId() == R.id.btn_Email_OrderActivity) {
+
+            Intent emailIntent = new Intent(Intent.ACTION_SEND);
+            emailIntent.setType("text/plain");
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your order from MyCafeApp");
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message: Information about order");
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"mycafeapp@company.com"});
+
+
+            if(emailIntent.resolveActivity(getPackageManager()) == null){//verify whether the device can receive the following intent
+                Toast.makeText(OrderDetailActivity.this, "Sorry, no App can handle this action and data", Toast.LENGTH_SHORT).show();
+            }else{
+                startActivity(emailIntent);
+            }
+        }
     }
 }
